7070369b10ad01d8dcc77104cf9a1c8af225ede7
lena li <liyuanlena@gmail.com>
2020-03-02 15:52:56 -0800

    data preprocessing for DCN
    as title

---
+++ data/deepCrossNet_dataPreprocess.py
@@ -0,0 +1,24 @@
+
+from util import *
+
+
+if __name__ == '__main__':
+    if not os.path.isdir('train_data'):
+        os.mkdir('train_data')
+    if not os.path.isdir('test_data'):
+        os.mkdir('test_data')
+    if not os.path.isdir('aid_data'):
+        os.mkdir('aid_data')
+
+    data_size = 5
+    if os.path.exists('subsampled_raw_data/subsampled_train_' + str(data_size) + ".txt"):
+        os.remove('subsampled_raw_data/subsampled_train_' + str(data_size) + ".txt") 
+    if os.path.exists('train_data/train.txt'):
+        os.remove('train_data/train.txt') 
+    if os.path.exists('test_data/test.txt'):
+        os.remove('test_data/test.txt') 
+
+    subsample_raw_data(data_size)    
+    split_data(data_size)
+    get_feat_dict()
+    print('Done!')
